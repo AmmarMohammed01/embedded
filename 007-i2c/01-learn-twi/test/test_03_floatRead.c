b945fa99a8c14de1bd839e1149e125a6cb2a2c33
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2026-08-30 18:21:46 -0500

    Project #007 I2C: Printing fraction numbers. Dividing up the raw data into different parts. Printing the whole number and fraction number separately. Bug with 0.0625 since multiplying by 10000 is 625 instead of 0625.

--- 007-i2c/01-learn-twi/test/test_03_floatRead.c
+++ 007-i2c/01-learn-twi/test/test_03_floatRead.c
@@ -3,6 +3,7 @@
 
 unsigned shift_first_bits(unsigned data, unsigned nBits);
 unsigned mask_last_bits(unsigned data, unsigned nBits);
+void printBits_unsigned(unsigned char * bytes);
 
 int main() {
 	printf("float: %zu\n", sizeof(float));
@@ -14,8 +15,7 @@ int main() {
 	}
 	*/
 
-	unsigned char * bytes = (unsigned char *)&num;
-	char currentBit;
+	unsigned char * bytes = (unsigned char *) &num;
 
 	//With this code I found out my system (MacBook) stores bytes in little endian order. What does ATmega328p use?
 	//10001111 00001100 10011011 01000101 = 0x 8F 0C 9B 45 <- little endian
@@ -33,13 +33,7 @@ int main() {
 		printf(" ");
 	}
 	*/
-	for(int byte = sizeof(float) - 1; byte >= 0; byte--) {
-		for(int bit = 7; bit >= 0; bit--) {
-			currentBit = bytes[byte] & (1 << bit) ? '1' : '0';
-			printf("%c", currentBit);
-		}
-		printf(" ");
-	}
+	printBits_unsigned((unsigned char *)&num);
 
 	//DIVIDE UP float num
 	//SIGN
@@ -55,7 +49,7 @@ int main() {
 	biasedExponent |= (bytes[2] & 0x80) >> 7; // first 1 bit of second float byte
 	printf("Biased Exponent: ");
 	for(int bit = 7; bit >= 0; bit--) {
-		currentBit = biasedExponent & (1 << bit) ? '1' : '0';
+		char currentBit = biasedExponent & (1 << bit) ? '1' : '0';
 		printf("%c", currentBit);
 	}
 	printf("\n");
@@ -86,15 +80,7 @@ int main() {
 	//unsigned mantissa = (bytes[2] & 0x7F) | bytes[1] | bytes[0];
 	unsigned mantissa = (1 << 23) | ((bytes[2] & 0x7F) << 16) | (bytes[1] << 8) | bytes[0];
 	printf("Mantissa bits: ");
-	bytes = (unsigned char *) &mantissa;
-	for(int byte = sizeof(unsigned) - 1; byte >= 0; byte--) {
-		for(int bit = 7; bit >= 0; bit--) {
-			currentBit = bytes[byte] & (1 << bit) ? '1' : '0';
-			printf("%c", currentBit);
-		}
-		printf(" ");
-	}
-	printf("\n");
+	printBits_unsigned((unsigned char *)&mantissa);
 
 	//somehow I need to left shfit mantissa by exponent amount
 	//10011011 00001100 10001111 ; this has implicit 1, if I left shift by exponent w/ implicit 1, I should add one to exponent
@@ -113,26 +99,30 @@ int main() {
 	printf("Whole Num: %d\n", wholeNum);
 
 	//print whole num bits
-	bytes = (unsigned char *) &wholeNum;
-	for(int byte = sizeof(unsigned) - 1; byte >= 0; byte--) {
-		for(int bit = 7; bit >= 0; bit--) {
-			currentBit = bytes[byte] & (1 << bit) ? '1' : '0';
-			printf("%c", currentBit);
-		}
-		printf(" ");
-	}
-	printf("\n");
+	printBits_unsigned((unsigned char *)&wholeNum);
 
 	printf("Fraction: %d\n", fraction);
-	bytes = (unsigned char *) &fraction;
-	for(int byte = sizeof(unsigned) - 1; byte >= 0; byte--) {
-		for(int bit = 7; bit >= 0; bit--) {
-			currentBit = bytes[byte] & (1 << bit) ? '1' : '0';
-			printf("%c", currentBit);
-		}
-		printf(" ");
-	}
-	printf("\n");
+	printBits_unsigned((unsigned char *)&fraction);
+
+	//interpret fraction bits. turn the fraction bits into an unsigned number.
+	//TMP1075 only uses a max of 4 fraction bits. Wait, I probably could have just converted the 4 fraction bits if I just wanted to print.
+	//The reason I did all of this, I already converted raw TMP1075 data to float, I needed a way to print the float.
+	//0.5, 0.25, 0.125, 0.0625 -> would look like 5000, 2500, 1250, & 0625 when printed but a '.' will be sent before these values are printed
+
+	//Current unsigned fraction bits = 00000000 00000000 00000100 10001111
+	//I need a way to identify the first fraciton bit.
+
+	/*
+	 * I did work on trying to convert a float to a string for a substantial amount of time over these past 2-3 days (8/28-29-30/2026)
+	 * Here is my realization:
+	 * 1. I could convert the fractions bits into one unsigned integer and print it, limited to 4 bytes.
+	 * 	- 2^(32) - 1 = 4,294,967,295. Since first digit only climbs to 4, discard the 10th digit. Leaves 9 digits to store floating point as unsigned int.
+	 * 2. I could store the digits from the fraction bits as Binary Coded Decimal, BCD, which is 1 byte per digit
+	 * 	- TMP1075 only needs to represent 4 floating point digits, so 4 bytes for 4 characters instead of 9.
+	 * 3. I could convert the whole number to string first. Then multiply the fraction portion by a factor of 10 to get it as a integer to convert to string.
+	 * 	- Is there a way to find how many decimal digits are in a floating point number?
+	 * 	- There is a way to find how many digits are in an integer using %10 and /10.
+	*/
 
 	return 0;
 }
@@ -147,4 +137,16 @@ unsigned mask_last_bits(unsigned data, unsigned nBits) {
 	printf("nBits: %u\n", nBits);
 	unsigned maskedData = data & ((1u << nBits) - 1u);
 	return maskedData;
+}
+
+void printBits_unsigned(unsigned char * bytes) {
+	char currentBit;
+	for(int byte = sizeof(unsigned) - 1; byte >= 0; byte--) {
+		for(int bit = 7; bit >= 0; bit--) {
+			currentBit = bytes[byte] & (1 << bit) ? '1' : '0';
+			printf("%c", currentBit);
+		}
+		printf(" ");
+	}
+	printf("\n");
 }
---
+++ 007-i2c/01-learn-twi/test/test_04_tmp1075raw_to_str.c
@@ -0,0 +1,130 @@
+// 8/30/2026
+
+#include <stdio.h>
+#include <stdint.h>
+#include "helper.h"
+
+typedef struct TMP1075_Info {
+	uint8_t sign;
+	uint8_t wholeNum;
+	uint16_t fractionNum;
+} tmp1075_info_t;
+
+//float tmp1075_raw_to_celsius(uint16_t rawData);
+tmp1075_info_t tmp1075_raw_divide(uint16_t rawData);
+
+int main() {
+	char firstNum[5];
+	char secondNum[5];
+
+	tmp1075_info_t rinfoMain = tmp1075_raw_divide(0x7FF0);
+	if(rinfoMain.sign == 1) { printf("-"); }
+	intToString((uint16_t)rinfoMain.wholeNum, firstNum);
+	intToString(rinfoMain.fractionNum, secondNum);
+	printf("%s.%s'C\n", firstNum, secondNum);
+
+	rinfoMain = tmp1075_raw_divide(0x8000);
+	if(rinfoMain.sign == 1) { printf("-"); }
+	intToString((uint16_t)rinfoMain.wholeNum, firstNum);
+	intToString(rinfoMain.fractionNum, secondNum);
+	printf("%s.%s'C\n", firstNum, secondNum);
+
+	rinfoMain = tmp1075_raw_divide(0x0010);
+	if(rinfoMain.sign == 1) { printf("-"); }
+	intToString((uint16_t)rinfoMain.wholeNum, firstNum);
+	intToString(rinfoMain.fractionNum, secondNum);
+	//if(rinfoMain.fractionNum < 1000 && rinfoMain.fractionNum > 99) { printf("0"); }
+	printf("%s.%s'C\n", firstNum, secondNum);
+
+	rinfoMain = tmp1075_raw_divide(0x0020);
+	if(rinfoMain.sign == 1) { printf("-"); }
+	intToString((uint16_t)rinfoMain.wholeNum, firstNum);
+	intToString(rinfoMain.fractionNum, secondNum);
+	printf("%s.%s'C\n", firstNum, secondNum);
+
+	rinfoMain = tmp1075_raw_divide(0x0040);
+	if(rinfoMain.sign == 1) { printf("-"); }
+	intToString((uint16_t)rinfoMain.wholeNum, firstNum);
+	intToString(rinfoMain.fractionNum, secondNum);
+	printf("%s.%s'C\n", firstNum, secondNum);
+
+	return 0;
+}
+
+//previous
+/*
+float tmp1075_raw_to_celsius(uint16_t rawData) {
+	uint8_t magnitude;
+	uint8_t fraction;
+	rawData = rawData >> 4;
+
+	float sign = 1;
+	if(rawData & 0x0800) { // or (rawData & (1 << 11)), which is better to show masking?
+		sign = -1;
+
+		rawData -= 1;
+		rawData = ~rawData;
+	}
+	
+	magnitude = (uint8_t)((rawData & 0x0FF0) >> 4);
+	fraction = (uint8_t)(rawData & 0x000F);
+
+	float celsius = (float)magnitude;
+	
+	float fractionSum = 0;
+	if(fraction & (1 << 3)) {
+		fractionSum += 0.5;
+	} else if (fraction & (1 << 2)) {
+		fractionSum += 0.25;
+	} else if (fraction & (1 << 1)) {
+		fractionSum += 0.125;
+	} else if (fraction & (1 << 0)) {
+		fractionSum += 0.0625;
+	}
+
+	celsius += fractionSum;
+	celsius *= sign;
+	return celsius;
+}
+*/
+
+tmp1075_info_t tmp1075_raw_divide(uint16_t rawData) {
+	uint8_t magnitude, fraction;
+	rawData = rawData >> 4;
+
+	uint8_t sign = 0;
+	if(rawData & 0x0800) { // or (rawData & (1 << 11)), which is better to show masking?
+		sign = 1;
+
+		rawData -= 1;
+		rawData = ~rawData;
+	}
+	
+	magnitude = (uint8_t)((rawData & 0x0FF0) >> 4);
+	fraction = (uint8_t)(rawData & 0x000F);
+
+	float fractionSum = 0;
+	if(fraction & (1 << 3)) {
+		fractionSum += 0.5;
+	}
+	if (fraction & (1 << 2)) {
+		fractionSum += 0.25;
+	}
+	if (fraction & (1 << 1)) {
+		fractionSum += 0.125;
+	}
+	if (fraction & (1 << 0)) {
+		fractionSum += 0.0625;
+	}
+
+	uint16_t fractionDigits = fractionSum * 10000; //0.0625 * 10000 = 625, I need 0625 to print
+	printf("%u\n", fractionDigits);
+
+	tmp1075_info_t rinfo1 = {
+		.sign = sign,
+		.wholeNum = magnitude,
+		.fractionNum = fractionDigits
+	};
+
+	return rinfo1;
+}
